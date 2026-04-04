9f7d8477a8aaaa2b753b55c90f75826cb617a2b1
mikeyperes <michael@mike-ro-tech.com>
2026-04-04 19:03:11 -0400

    feat: add DocumentationService registration in boot()

--- src/Providers/WpToolkitServiceProvider.php
+++ src/Providers/WpToolkitServiceProvider.php
@@ -44,5 +44,12 @@ class WpToolkitServiceProvider extends ServiceProvider
             $registry = app(PackageRegistryService::class);
             $registry->registerSidebarLink('wptoolkit.index', 'WP Toolkit', 'M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.066 2.573c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.573 1.066c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.066-2.573c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z M15 12a3 3 0 11-6 0 3 3 0 016 0z', 'Sandbox', 'wptoolkit', 88);
         }
-    }
+    
+        // Documentation
+        if (class_exists(\hexa_core\Services\DocumentationService::class)) {
+            app(\hexa_core\Services\DocumentationService::class)->register('wptoolkit', 'WP Toolkit', 'hexawebsystems/laravel-hexa-package-wptoolkit', [
+                ['title' => 'Overview', 'content' => '<p>WordPress Toolkit integration. Manage WP installs, credentials, auto-login, password reset via wp-toolkit CLI.</p>'],
+            ]);
+        }
+}
 }
