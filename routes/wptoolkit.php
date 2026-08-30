1055de6b033c397f7cb11c06e652ac66cbc296a6
mikeyperes <mikeyperes@users.noreply.github.com>
2026-08-30 18:31:07 +0000

    security: constrain toolkit routes 3.2.1

--- routes/wptoolkit.php
+++ routes/wptoolkit.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use hexa_package_wptoolkit\Http\Controllers\WpToolkitDashboardController;
 
-Route::middleware(['web', 'auth', 'locked', 'system_lock', 'role'])->group(function () {
+Route::middleware(['web', 'auth', 'locked', 'system_lock', 'two_factor', 'role'])->group(function () {
     Route::get('wp-toolkit', [WpToolkitDashboardController::class, 'index'])->name('wptoolkit.index');
     Route::hexaRawPage('raw-wp-toolkit', [WpToolkitDashboardController::class, 'raw'], 'wptoolkit.raw', [
         'package' => 'wptoolkit',
